e07f717ed4bca0d9043eb620b7f4739cbcc5bb25
kasoccc <vihithastratign@gmail.com>
2025-08-20 09:14:51 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,7 @@ function App() {
 
   const handleClick = async () => {
     try {
-      const res = await fetch("http://localhost:3000/api/hi", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ message: "hi" }),
-      });
+      const res = await fetch("https://3af7852d2e7c.ngrok-free.app/testme?name=HarryPoter");
       const text = await res.text();
       setResponse(text);
     } catch (err) {
